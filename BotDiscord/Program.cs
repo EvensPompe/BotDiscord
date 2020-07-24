2f3512349271660d4dc582f0fca77d74e05ec0d2
Evens Pompe <evenspompe9593@gmail.com>
2020-07-24 23:59:29 +0200

    Ajout de l'api google/youtube + commande !manger

--- BotDiscord/Program.cs
+++ BotDiscord/Program.cs
@@ -1,14 +1,27 @@
 ﻿using System;
 using System.Threading.Tasks;
+using System.Configuration;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Threading;
+
 using Discord;
 using Discord.WebSocket;
-using System.Configuration;
+
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Services;
+using Google.Apis.Upload;
+using Google.Apis.Util.Store;
+using Google.Apis.YouTube.v3;
+using Google.Apis.YouTube.v3.Data;
 
 namespace BotDiscord
 {
     class Program
     {
         private DiscordSocketClient _client;
+        private string[] foodArray;
         public static void Main(string[] args)
             => new Program().MainAsync().GetAwaiter().GetResult();
         public async Task MainAsync()
@@ -35,7 +48,6 @@ namespace BotDiscord
 
         private async Task MessageUpdated(Cacheable<IMessage, ulong> before, SocketMessage after, ISocketMessageChannel channel)
         {
-            // If the message was not in the cache, downloading it will result in getting a copy of `after`.
             var message = await before.GetOrDownloadAsync();
             Console.WriteLine($"{message} -> {after}");
         }
@@ -66,6 +78,39 @@ namespace BotDiscord
             {
                 var emoji = new Emoji("\uD83E\uDDC0");
                 await message.AddReactionAsync(emoji);
+            }
+
+            if (message.Content.ToLower() == "!manger")
+            {
+                var youtubeService = new YouTubeService(new BaseClientService.Initializer()
+                {
+                    ApiKey = Environment.GetEnvironmentVariable("GOOGLE_API_KEY"),
+                    ApplicationName = this.GetType().ToString()
+                });
+
+                foodArray = new string[] { "Sushi", "Burger", "Ramen", "Tacos", "Jambon Beurre" };
+
+                int randomFood = new Random().Next(foodArray.Length);
+                string food = foodArray[randomFood];
+
+                var searchListRequest = youtubeService.Search.List("snippet");
+                searchListRequest.Q = food;
+                searchListRequest.MaxResults = 50;
+
+                var searchListResponse = await searchListRequest.ExecuteAsync();
+
+                int randomIndex = new Random().Next(50);
+                int index = 0;
+
+                foreach (var searchResult in searchListResponse.Items)
+                {
+                    if (index == randomIndex && searchResult.Id.Kind == "youtube#video")
+                    {
+                            string resFoodVideo = "https://www.youtube.com/watch?v=" + searchResult.Id.VideoId;
+                            await message.Channel.SendMessageAsync(resFoodVideo);
+                    }
+                    index ++;
+                }
             }
         }
     }
