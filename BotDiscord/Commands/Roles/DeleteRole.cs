eae29a1bed436a61954b2be370c2bf9d040ba888
Evens Pompe <evenspompe9593@gmail.com>
2023-11-01 22:13:37 +0100

    feat(Commands): add DeleteRole command

---
+++ BotDiscord/Commands/Roles/DeleteRole.cs
@@ -0,0 +1,28 @@
+﻿using System.Threading.Tasks;
+using System.Linq;
+
+using Discord.Commands;
+
+
+
+namespace BotDiscord.Commands.Roles
+{
+    public class DeleteRole : ModuleBase<SocketCommandContext>
+    {
+        [Command("deleterole")]
+        public async Task DeleteRoleAsync(string roleWanted)
+        {
+            var roles = Context.Guild.Roles;
+            var role = roles.FirstOrDefault(v => v.Name == roleWanted);
+            if (role != null)
+            {
+                await role.DeleteAsync();
+                await ReplyAsync($"Le rôle {roleWanted} a été supprimé !");
+            }
+            else
+            {
+                await ReplyAsync("Le rôle n'existe pas !");
+            }
+        }
+    }
+}
